808084cb93dd077ef046e404230a1eb8344fd83d
Jordan Shehane <56180519+0neMiss@users.noreply.github.com>
2021-07-05 11:46:22 -0400

    header with buttons, info card

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+      <div class="p-6 mx-auto bg-gray-400  shadow-lg flex justify-center space-between ">
+        <div class="space-x-14">
+            <button class="text-xl font-mono  text-gray-50">Home</button>
+            <button class="text-xl font-mono text-gray-50">Components</button>
+            <button class="text-xl font-mono text-gray-50">Resume</button>
+            <button class="text-xl font-mono text-gray-50">Contact</button>
+        </div>
+      </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Introduction.js
@@ -0,0 +1,13 @@
+const Introduction = () => {
+    return (
+        <div class ="flex justify-center ">
+            <div class="p-8 my-28 mx-96 max-w-lg bg-gray-200 shadow-lg place-content-center flex-col justify-center rounded-lg">
+                <img src="https://avatars.githubusercontent.com/u/56180519?v=4" class="rounded-full max-h-40 mb-5 ml-auto mr-auto"></img>
+                    <p class="text-lg">Hi I'm Jordan Shehane, a web developer based out of Newport News Virginia. I was working in device repair and I loved it, but it turns out I love software a lot more. I wanted to make a career change, so I ended up attending part time classes at Lambda School. My specialties are MERN and SERN stacks but I have experience with a lot of different technologies. In the components section you can find a library of different animations, buttons, and other frontend components I've built, and I've included links to the code as well as what different libraries I used to make them.  Outside of programming I like to rock climb, and I play jazz piano. </p>
+            </div>
+        </div>
+    );
+  };
+  
+  export default Introduction;
+  
