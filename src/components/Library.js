1a314eb7c4e77417893c21b429a803756611bf9f
Jordan Shehane <56180519+0neMiss@users.noreply.github.com>
2021-07-05 20:21:38 -0400

    set up base routes and components

---
+++ src/components/Library.js
@@ -0,0 +1,10 @@
+const Library = () => {
+    return (
+    <div class="h-screen bg-cover overflow-hidden bg-gray-500">
+        <p class="mt-96" >hi</p>
+    </div>
+    );
+  };
+  
+  export default Library;
+  
